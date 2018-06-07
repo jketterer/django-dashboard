e48f06795e43ead3b950dcd3235706f79cdf0b5a
Jeremy Ketterer <j.ketterer1314@gmail.com>
2018-06-06 21:41:33 -0400

    fixed fata bug occurring between 00:00:00 and 03:00:00

--- board/views.py
+++ board/views.py
@@ -17,35 +17,67 @@ def index(request):
 
     # put all values in an array
     cnt = 0
-    for i in range(0, 5): # run once for each day
+    for i in range(0, 5):  # run once for each day
+
+        # if there is no data left for current day, copy first section from next day
+        if i == 0 & city_weather['list'][cnt]['dt_txt'].find("03:00:00") == 0:
+            highs[i].append(city_weather['list'][cnt]['main']['temp_max'])
+            continue
+
         # run through each 3hr section until new day
         while city_weather['list'][cnt]['dt_txt'].find("03:00:00") == -1:
+            # adds info to day
             highs[i].append(city_weather['list'][cnt]['main']['temp_max'])
             cnt += 1
+
+        # required to add the 03:00:00 data set
+        highs[i].append(city_weather['list'][cnt]['main']['temp_max'])
+
         cnt += 1
 
     # same loop for lows
     cnt = 0
     for i in range(0, 5):
+
+        if i == 0 & city_weather['list'][cnt]['dt_txt'].find("03:00:00") == 0:
+            lows[i].append(city_weather['list'][cnt]['main']['temp_min'])
+            continue
+
         while city_weather['list'][cnt]['dt_txt'].find("03:00:00") == -1: # 3AM for EST
             lows[i].append(city_weather['list'][cnt]['main']['temp_min'])
             cnt += 1
+
+        lows[i].append(city_weather['list'][cnt]['main']['temp_min'])
         cnt += 1
 
     # same loop for descriptions
     cnt = 0
     for i in range(0, 5):
+
+        if i == 0 & city_weather['list'][cnt]['dt_txt'].find("03:00:00") == 0:
+            descs[i].append(city_weather['list'][cnt]['weather'][0]['main'])
+            continue
+
         while city_weather['list'][cnt]['dt_txt'].find("03:00:00") == -1:
             descs[i].append(city_weather['list'][cnt]['weather'][0]['main'])
             cnt += 1
+
+        descs[i].append(city_weather['list'][cnt]['weather'][0]['main'])
         cnt += 1
 
     # same loop for icon codes
     cnt = 0
     for i in range(0, 5):
+
+        if i == 0 & city_weather['list'][cnt]['dt_txt'].find("03:00:00") == 0:
+            codes[i].append(city_weather['list'][cnt]['weather'][0]['id'])
+            continue
+
         while city_weather['list'][cnt]['dt_txt'].find("03:00:00") == -1:
             codes[i].append(city_weather['list'][cnt]['weather'][0]['id'])
             cnt += 1
+
+        codes[i].append(city_weather['list'][cnt]['weather'][0]['id'])
         cnt += 1
 
     # functions to calculate max/min temp and most common description
@@ -66,15 +98,15 @@ def index(request):
         return low
 
     def get_description(arr):
-        counter = collections.Counter(arr) # put array into fancy counter
-        counter = counter.most_common(1) # get most common value
-        clean = (re.sub('[^a-zA-Z]+', '', str(counter[0]))) # remove extras
+        counter = collections.Counter(arr)  # put array into fancy counter
+        counter = counter.most_common(1)  # get most common value
+        clean = (re.sub('[^a-zA-Z]+', '', str(counter[0])))  # remove extras
         return clean
 
     def get_code(arr):
         counter = collections.Counter(arr)
         counter = counter.most_common(1)
-        clean = str(counter)[2:5] # extract 3 digit code from output
+        clean = str(counter)[2:5]  # extract 3 digit code from output
         return clean
 
     weather = {
